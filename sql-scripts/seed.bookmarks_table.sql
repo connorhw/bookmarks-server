b07c78175f333a862731b6751312dc58ce0416aa
connorhw <connorwong93@gmail.com>
2020-07-19 03:31:20 -0700

    Adds seeding script successfully.

---
+++ sql-scripts/seed.bookmarks_table.sql
@@ -0,0 +1,7 @@
+INSERT INTO bookmarks_table (title, url, description, rating)
+VALUES
+    ('title1seed', 'url1.comseed', 'description1seed', 1),
+    ('title2seed', 'url2.comseed', 'description2seed', 2),
+    ('title3seed', 'url3.comseed', 'description3seed', 3),
+    ('title4seed', 'url4.comseed', 'description4seed', 4)
+;
