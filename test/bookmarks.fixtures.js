89547576eda63fb0895b45a135b1a13dbb56e8be
connorhw <connorwong93@gmail.com>
2020-11-08 18:02:45 -0500

    trying to finish POST and DELETE with PostgreSQL section. Code not working.

--- test/bookmarks.fixtures.js
+++ test/bookmarks.fixtures.js
@@ -1,36 +1,50 @@
 function makeBookmarksArray() {
     return [
-        {
-            id: 1,
-            title: 'title 1',
-            url: 'www.1.com',
-            description: 'desc 1',
-            rating: 1,
-        },
-        {
-            id: 2,
-            title: 'title 2',
-            url: 'www.2.net',
-            description: 'desc 2',
-            rating: 22,
-        },
-        {
-            id: 3,
-            title: 'title 3',
-            url: 'www.3.org',
-            description: 'desc 3',
-            rating: 333,
-        },
-        {
-            id: 4,
-            title: 'title 4',
-            url: 'www.4.edu',
-            description: 'desc 4',
-            rating: 4444,
-        },
-    ];
-}
-
-module.exports = {
+      {
+        id: 1,
+        title: 'Thinkful',
+        url: 'https://www.thinkful.com',
+        description: 'Think outside the classroom',
+        rating: 5,
+      },
+      {
+        id: 2,
+        title: 'Google',
+        url: 'https://www.google.com',
+        description: 'Where we find everything else',
+        rating: 4,
+      },
+      {
+        id: 3,
+        title: 'MDN',
+        url: 'https://developer.mozilla.org',
+        description: 'The only place to find web documentation',
+        rating: 5,
+      },
+    ]
+  }
+  
+  function makeMaliciousBookmark() {
+    const maliciousBookmark = {
+      id: 911,
+      title: 'Naughty naughty very naughty <script>alert("xss");</script>',
+      url: 'https://www.hackers.com',
+      description: `Bad image <img src="https://url.to.file.which/does-not.exist" onerror="alert(document.cookie);">. But not <strong>all</strong> bad.`,
+      rating: 1,
+    }
+    const expectedBookmark = {
+      ...maliciousBookmark,
+      title: 'Naughty naughty very naughty &lt;script&gt;alert(\"xss\");&lt;/script&gt;',
+      description: `Bad image <img src="https://url.to.file.which/does-not.exist">. But not <strong>all</strong> bad.`
+    }
+    return {
+      maliciousBookmark,
+      expectedBookmark,
+    }
+  }
+  
+  module.exports = {
     makeBookmarksArray,
-}
+    makeMaliciousBookmark,
+  }
+  
--- test/setup.js
+++ test/setup.js
@@ -1,5 +1,7 @@
-require('dotenv').config()
+process.env.TZ = 'UTC'
+process.env.NODE_ENV = 'test'
 
+require('dotenv').config()
 const { expect } = require('chai')
 const supertest = require('supertest')
 
